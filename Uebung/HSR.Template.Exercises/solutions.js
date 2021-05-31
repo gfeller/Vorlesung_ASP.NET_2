0a49bb77ebc7c8cdec5c8500e58a1523f5571627
Michael Gfeller <michael.gfeller@ost.ch>
2021-05-31 15:03:25 +0200

    update übung

--- Uebung/HSR.Template.Exercises/solutions.js
+++ Uebung/HSR.Template.Exercises/solutions.js
@@ -1,11 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var body = document.getElementsByTagName('body')[0];
-	if(window.location.search.match( /show-solutions/gi )) {
-		body.setAttribute('data-show-solutions','true');
-	}
-
-	var showSolutionsButton = document.getElementById('showSolutions');
+	showSolutionsButton = document.getElementById('showSolutions');
 	if(showSolutionsButton) {
+		body = document.getElementsByTagName('body')[0];
 		showSolutionsButton.addEventListener('click', function() {
 			if(body.hasAttribute('data-show-solutions')) {
 				body.removeAttribute('data-show-solutions');
