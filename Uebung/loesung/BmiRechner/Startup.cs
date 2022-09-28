8437ca50193864182171b3e9af5fdd32b66e34e8
Michael Gfeller <michael.gfeller@ost.ch>
2022-09-28 11:59:00 +0200

    net6

--- Uebung/loesung/BmiRechner/Startup.cs
+++ Uebung/loesung/BmiRechner/Startup.cs
@@ -29,13 +29,7 @@ namespace BmiRechner
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
+    
 
             services.AddMvc();
             services.AddSingleton<IBmiService, BmiService>();
@@ -50,16 +44,7 @@ namespace BmiRechner
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+         
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -77,6 +62,7 @@ namespace BmiRechner
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapRazorPages();
             });
         }
     }
